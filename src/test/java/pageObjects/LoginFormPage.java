4379ccaaee97aa2985191e36cb28a655bb4dba44
Darya <darya.slinkova@gmail.com>
2025-05-04 23:13:01 +0300

    Add LoginData to train Builder with Lombok Refactor LoginFormTests to Fluent POM

--- src/test/java/pageObjects/LoginFormPage.java
+++ src/test/java/pageObjects/LoginFormPage.java
@@ -1,9 +1,47 @@
 package pageObjects;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginFormPage extends BasePage {
+    @FindBy(id = "username")
+    private WebElement usernameInputField;
+
+    @FindBy(id = "password")
+    private WebElement passwordInputField;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement submitButton;
+
+    @FindBy(className = "alert")
+    private WebElement resultMessage;
+
     public LoginFormPage(WebDriver driver) {
         super(driver);
+    }
+
+    @Step("Enter username")
+    public LoginFormPage enterUsername(String username) {
+        usernameInputField.sendKeys(username);
+        return this;
+    }
+
+    @Step("Enter password")
+    public LoginFormPage enterPassword(String password) {
+        passwordInputField.sendKeys(password);
+        return this;
+    }
+
+    @Step("Click submit button")
+    public LoginFormPage clickSubmit() {
+        submitButton.click();
+        return this;
+    }
+
+    @Step("Get the result message")
+    public String getResultMessage() {
+        return resultMessage.getText();
     }
 }
---
+++ src/test/java/tests/LoginFormTests.java
@@ -0,0 +1,65 @@
+package tests;
+
+import constants.Constants;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import pageObjects.HomePage;
+import pageObjects.LoginFormPage;
+import patterns.builder.LoginData;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LoginFormTests extends BaseTest {
+    LoginData loginData;
+
+    @Test
+    void validCredentialsTest() {
+        final String expectedMessage = "Login successful";
+
+        loginData = LoginData.builder()
+                .username(testConfig.getUsername())
+                .password(testConfig.getPassword())
+                .build();
+
+        String actualMessage = new HomePage(getDriver())
+                .navigateToPage(Constants.LOGIN_FORM_PAGE_PATH, LoginFormPage.class)
+                .enterUsername(loginData.getUsername())
+                .enterPassword(loginData.getPassword())
+                .clickSubmit()
+                .getResultMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    static Stream<Arguments> invalidCredentials() {
+        return Stream.of(
+                Arguments.of("", ""),
+                Arguments.of("        ", "        "),
+                Arguments.of("username", "password")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidCredentials")
+    void invalidCredentialsTest(String username, String password) {
+        final String expectedMessage = "Invalid credentials";
+
+        loginData = LoginData.builder()
+                .username(username)
+                .password(password)
+                .build();
+
+        String actualMessage = new HomePage(getDriver())
+                .navigateToPage(Constants.LOGIN_FORM_PAGE_PATH, LoginFormPage.class)
+                .enterUsername(username)
+                .enterPassword(password)
+                .clickSubmit()
+                .getResultMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+}
