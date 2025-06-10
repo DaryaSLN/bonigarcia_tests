9bb37c101773698dbe48c70229d0ed6c241a6258
Darya <darya.slinkova@gmail.com>
2025-06-10 23:15:27 +0300

    Improved remote start options

--- src/test/java/tests/BaseSelenideTest.java
+++ src/test/java/tests/BaseSelenideTest.java
@@ -1,8 +1,12 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import config.TestPropertiesConfig;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
@@ -12,6 +16,22 @@ abstract class BaseSelenideTest {
 
     @BeforeEach
     void setup() {
+        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+
+        if (remoteUrl != null) {
+            Configuration.remote = remoteUrl;
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+
+            Configuration.browserCapabilities = options;
+        }
+
         open(testConfig.getBaseUrl());
         getWebDriver().manage().window().maximize();
     }
